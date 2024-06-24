e8d90d6424a5726e10e1afd715c18b58bde29741
Fishback, Christopher J 1LT USARMY (USA) <christopher.fishback.mil@swf.army.mil>
2024-06-24 17:03:55 -0500

    initial project commit

---
+++ src/main/java/fish/see/LoF_Dashboard_v2/LoFDashboardV2Application.java
@@ -0,0 +1,13 @@
+package fish.see.LoF_Dashboard_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoFDashboardV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoFDashboardV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fish/see/LoF_Dashboard_v2/LoFDashboardV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fish.see.LoF_Dashboard_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoFDashboardV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
