316b8d34efe1484a8b02f092df9e9ffcb5fcc6ba
Fishback, Christopher J 1LT USARMY (USA) <christopher.fishback.mil@swf.army.mil>
2024-07-02 10:56:41 -0500

    admin log info

--- frontend/src/components/admin/Admin.tsx
+++ frontend/src/components/admin/Admin.tsx
@@ -1,5 +1,5 @@
 import {AdminProps} from "./scripts/AdminProps.ts";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import AdminTabs from "./AdminTabs.tsx";
 import ModifyTeams from "./modify teams/ModifyTeams.tsx";
 import ModifyWeekGames from "./modify week games/ModifyWeekGames.tsx";
@@ -9,11 +9,6 @@ function Admin(props: AdminProps) {
     const { teams, players} = props
     const [currentTab, setCurrentTab] = useState(0)
     const MODIFYTEAMS = 0
-
-    useEffect(() => {
-        console.log(teams)
-        console.log(players)
-    }, []);
 
     return (
         <>
--- frontend/src/components/admin/modify teams/ModifyTeams.tsx
+++ frontend/src/components/admin/modify teams/ModifyTeams.tsx
@@ -1,8 +1,15 @@
 import AddPlayerForm from "./forms/AddPlayerForm.tsx";
 import {AdminProps} from "../scripts/AdminProps.ts";
+import {useEffect} from "react";
 
 export default function ModifyTeams(props : AdminProps) {
     const {teams, players} = props
+
+    useEffect(() => {
+        console.log("Modify Teams")
+        console.log(teams)
+        console.log(players)
+    }, []);
 
     return (
         <>
